21b4a7b6b7fb590c51e0c6824f2e36db85efb95d
bertan-arslan <bertanars97@gmail.com>
2025-07-24 17:58:05 +0300

    heroslider bg-url

--- src/components/home-components/FeaturedPosts.jsx
+++ src/components/home-components/FeaturedPosts.jsx
@@ -3,7 +3,7 @@ import FeaturedPostCard from "./CardFeaturedPost";
 export default function FeaturedPosts() {
   return (
     <section className="my-15 md:my-35">
-      <div className="mx-auto flex flex-col justify-center items-center font-[Montserrat]  w-[260px]  text-center gap-5 md:w-[692px]">
+      <div className="mx-auto flex flex-col justify-center items-center font-[Montserrat]  w-[260px]  text-center gap-5 md:w-[30vw]">
         <h6 className="text-sm text-[#23A6F0] font-bold">Practice Advice</h6>
         <h2 className="text-[40px] font-bold text-[#252B42] leading-[50px]">
           Featured Posts
