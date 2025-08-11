85d4e10b1de77c0b2fb90e7b8548e1b8b145f0c7
bertan-arslan <bertanars97@gmail.com>
2025-08-11 09:40:25 +0300

    test

--- src/components/home/CardProduct.jsx
+++ src/components/home/CardProduct.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import {products} from "../../data/products.json"
+import {products} from "../../products.json"
 
 export default function CardProduct() {
  
