3250862464ff529ec506710b275c2930ed96405e
bertan-arslan <bertanars97@gmail.com>
2025-08-11 09:55:52 +0300

    test vercel

--- src/components/home/CardFeaturedPost.jsx
+++ src/components/home/CardFeaturedPost.jsx
@@ -1,5 +1,35 @@
 import { ChevronRight, Clock2, MessageSquareText } from "lucide-react";
-import posts from "../../data/posts.js"
+
+
+const posts = [
+  {
+    id: 1,
+    imgUrl: "Posts/post-1.jpg",
+    title: "Loudest à la Madison #1 (L'integral)",
+    explanation:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "16 July 2025",
+    comment: 10,
+  },
+  {
+    id: 2,
+    imgUrl: "Posts/post-2.jpg",
+    title: "Loudest à la Madison #1 (L'integral)",
+    explanation:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "16 July 2025",
+    comment: 10,
+  },
+  {
+    id: 3,
+    imgUrl: "Posts/post-3.jpg",
+    title: "Loudest à la Madison #1 (L'integral)",
+    explanation:
+      "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+    date: "16 July 2025",
+    comment: 10,
+  },
+];
 
 export default function FeaturedPostCard() {
   return (
